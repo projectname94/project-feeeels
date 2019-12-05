29e07849fbbec37343e9ed0abbb2cff5cd85dd5e
Rahul S. Shinde <rshinde@mica.edu>
2019-12-05 18:37:54 -0500

    adding if statement to issue class if no fonts set

--- site/snippets/components/creditcolumns.php
+++ site/snippets/components/creditcolumns.php
@@ -1,4 +1,9 @@
-<section id="colophon" class="<?= $issueclass ?>">
+<?php if (isset($issueclass)): ?>
+  <?php $issue_class = $issueclass ?>
+<?php else: ?>
+  <?php $issue_class = '' ?>
+<?php endif ?>
+<section id="colophon" class="<?= $issue_class ?>">
 	<?php if($credit_title != ''): ?>
 		<?= '<h1>'.$credit_title.'</h1>' ?>
 	<?php endif; ?>
@@ -6,7 +11,7 @@
   <?php foreach($credit_columns->toStructure() as $credit_column): ?>
     <div class="colophon_section">
       <h2 class="colophon_heading"><?= $credit_column->column_title()  ?></h2>
-      <section class="colophon_content <?= $issueclass ?>">
+      <section class="colophon_content <?= $issue_class ?>">
         <?= $credit_column->column_contents()->kt() ?>
       </section>
     </div>
