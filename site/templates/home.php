d3b5fd9ddaf83465adb4ffa348685eded55e41e3
Lukas Eigler-Harding <eiglerhardingl@gmail.com>
2019-12-12 16:54:31 -0500

    entry updates

--- site/templates/home.php
+++ site/templates/home.php
@@ -28,14 +28,38 @@
 <!-- footer -->
 	<footer>
 		<section class="col_2">
-			<a class="primary_button" href="https://www.instagram.com/feeeels.mag/?hl=en" target="_blank">Instagram</a>
-			<a class="primary_button" href="https://www.instagram.com/feeeels.mag/?hl=en" target="_blank">Shop</a>
+			<?php if($site->footer_links()->isNotEmpty()): ?>
+				<?php foreach($site->footer_links()->toStructure() as $footer_link): ?>
+					<a class="primary_button" href="<?= $footer_link->link_url() ?>" target="_blank"><?= $footer_link->link_name() ?></a>
+				<?php endforeach; ?>
+			<?php endif; ?>
 		</section>
 		<section class="col_2">
-			<form>
-				<input type="text" name="email" placeholder="Email Address">
-				<input type="submit" value="Subscribe" class="Subscribe">
-			</form>
+
+			<!-- Begin Mailchimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form action="https://feeeels.us20.list-manage.com/subscribe/post?u=1c9497c9e1d72f45053d96b85&amp;id=e6d4577fca" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+						<div class="mc-field-group">
+							<input type="email" value="" placeholder="Email Address" name="EMAIL" class="required email" id="mce-EMAIL">
+						</div>
+						<div class="mc-field-group">
+							<input type="text" value="" placeholder="First Name" name="FNAME" class="" id="mce-FNAME">
+						</div>
+					<div class="mc-field-group">
+						<input type="text" value="" placeholder="Last Name" name="LNAME" class="" id="mce-LNAME">
+					</div>
+					<div id="mce-responses" class="clear">
+					<div class="response" id="mce-error-response" style="display:none"></div>
+					<div class="response" id="mce-success-response" style="display:none"></div>
+				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1c9497c9e1d72f45053d96b85_e6d4577fca" tabindex="-1" value=""></div>
+				    <div class="clear"><input type="submit" class="Subscribe" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				    </div>
+				</form>
+			</div>
+			<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+			<!--End mc_embed_signup-->
 			<p>Follow updates for details on pre-sale and limited edition merch.</p>
 	</section>
 	</footer>
